f1bbd8a28b5d7e80e3e24fe03a6652baef142cbb
GigiLi89 <li.gigi89@gmail.com>
2024-06-20 03:26:09 +0000

    had to update pycopg2

--- memobook_drfapi/settings.py
+++ memobook_drfapi/settings.py
@@ -152,7 +152,14 @@ DATABASES = {
     }
 }
 
-if 'DEV' not in os.environ:
+if 'DEV' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+else:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
     }
